32906b1c11812344541d67e6441c88f77aaab4fc
Jaehyun Bhang <ryanb@Jaehyunui-MacBookPro.local>
2024-03-25 21:14:37 +0900

    feat: apply inline styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,13 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      {/*Note that the 'text-align' in the style prop can be set as textAlign, which is recommanded */}
+      {/* Note: FlexBox aligns all its child to the middle of the paragraph. */}
+      {/* If you disable it, then you could see that the 'text-align: left' is applied. */}
+      <p style={{
+        color: 'magenta',
+        'text-align': 'left',
+      }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
