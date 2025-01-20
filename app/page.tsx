6a1d602c3f42baaa4196769880c1b9a8449f259b
calculus0129 <ryanbhang129@naver.com>
2025-01-20 13:03:54 +0900

    refactor: adjust layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 import AuthInputs from "@/component/AuthInputs";
 import Header from "@/component/Header";
 
