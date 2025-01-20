cd9b317e5789393036378c6c63c77d83c60ae337
calculus0129 <ryanbhang129@naver.com>
2025-01-20 12:41:35 +0900

    feat: add basic frontend

---
+++ component/AuthInputs.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState } from "react";
+
+type Policy = {
+  field: string;
+  content: string;
+  predicate: (value: string) => boolean;
+};
+
+const policies: Policy[] = [
+  {
+    field: "email",
+    content: "email should contain @",
+    predicate: (value: string) => value.includes("@"),
+  },
+  {
+    field: "password",
+    content: "password should be at least 6 characters",
+    predicate: (value: string) => value.trim().length < 6,
+  },
+];
+
+const AuthInputs = () => {
+  const [enteredContent, setEnteredContent] = useState<{
+    [key: string]: string;
+  }>({
+    email: "",
+    password: "",
+  });
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (field: string, value: string) => {
+    setEnteredContent((prevContent) => {
+      return {
+        ...prevContent,
+        [field]: value,
+      };
+    });
+  };
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+    // console.log(enteredEmail, enteredPassword);
+  };
+
+  const invalidPolicies = policies.filter((policy: Policy) => {
+    return !policy.predicate(enteredContent[policy.field])
+      ? policy.field
+      : undefined;
+  });
+
+  return (
+    <div id="auth-inputs">
+      <div className="controls">
+        <p>
+          <label>Email</label>
+          <input
+            type="email"
+            className={
+              invalidPolicies.filter((policy) => policy.field === "email")
+                ? "invalid"
+                : undefined
+            }
+            onChange={(event) => handleInputChange("email", event.target.value)}
+          />
+        </p>
+        <p>
+          <label>Password</label>
+          <input
+            type="password"
+            className={
+              invalidPolicies.filter((policy) => policy.field === "password")
+                ? "invalid"
+                : undefined
+            }
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <button className="button" onClick={handleSubmit}>
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AuthInputs;
